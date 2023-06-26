0eaab7bda145d0ea83bb6f9908494b80d85e203b
BuildTools <unconfigured@null.spigotmc.org>
2023-06-26 09:47:49 -0400

    Implemented Framer Motion onScroll functionality

--- src/pages/components/Introduction.tsx
+++ src/pages/components/Introduction.tsx
@@ -1,32 +1,81 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import React from "react";
 
 export default function Introduction() {
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  const slideIn = {
+    hidden: { x: 400, opacity: 0.2 },
+    visible: { x: 0, opacity: 1 },
+  };
+
+  const combination = {
+    hidden: { opacity: 0, x: 400 },
+    visible: { opacity: 1, x: 0 },
+  };
+  const [ref1, inView1] = useInView({
+    triggerOnce: true,
+    threshold: 0.6,
+  });
+
+  const [ref2, inView2] = useInView({
+    triggerOnce: true,
+    threshold: 0.6,
+  });
+
+  const [ref3, inView3] = useInView({
+    triggerOnce: true,
+    threshold: 0.6,
+  });
+
+  const [ref4, inView4] = useInView({
+    triggerOnce: true,
+    threshold: 0.6,
+  });
+
   return (
     <div>
       <div className="flex items-center justify-center">
-        <div
+        <motion.div
+          ref={ref1}
           className="relative flex w-2/5 flex-col items-start justify-center gap-4 bg-pink-100 px-20"
           style={{ height: "50vh" }}
+          variants={slideIn}
+          transition={{ duration: 1.0 }}
+          initial="hidden"
+          animate={inView1 ? "visible" : "hidden"}
         >
           <hr className="h-3 border-gray-300"></hr>
-          <h1 className="text-4xl font-light">
+          <motion.h1
+            className="text-4xl font-light"
+            variants={fadeIn}
+            transition={{ duration: 0.8 }}
+          >
             Welcome to Source Parent Health!
-          </h1>
-          <h5 className="text-2xl font-light">
+          </motion.h1>
+          <motion.h5
+            className="text-2xl font-light"
+            variants={fadeIn}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
             A new and upcoming business that is dedicated to making YOUR life
             more special
-          </h5>
-
-          <p
+          </motion.h5>
+          <motion.p
             className="tracking-wide text-gray-500"
             style={{ fontFamily: "montserrat" }}
+            variants={fadeIn}
+            transition={{ duration: 0.8, delay: 0.4 }}
           >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et
             justo sed
-          </p>
+          </motion.p>
           <div className="flex gap-4">
             <Link
               href="/courses"
@@ -41,7 +90,7 @@ export default function Introduction() {
               Learn More
             </Link>
           </div>
-        </div>
+        </motion.div>
       </div>
       <div
         className="flex items-center"
@@ -53,16 +102,29 @@ export default function Introduction() {
           backgroundPosition: "center",
         }}
       >
-        <div className="relative flex h-96 w-2/3 justify-center gap-4 p-8">
+        <motion.div
+          className="relative flex h-96 w-2/3 justify-center gap-4 p-8"
+          variants={slideIn}
+          initial="hidden"
+          animate="visible"
+        >
           <div className="relative w-1/4 flex-col">
-            <h1 className="font-cursive text-6xl font-light tracking-wide">
+            <motion.h1
+              className="font-cursive text-6xl font-light tracking-wide"
+              variants={fadeIn}
+              transition={{ duration: 0.8 }}
+            >
               Buy Our E-BOOK!
-            </h1>
+            </motion.h1>
             <br />
-            <p className="tracking-wide text-gray-600">
+            <motion.p
+              className="tracking-wide text-gray-600"
+              variants={fadeIn}
+              transition={{ duration: 0.8, delay: 0.2 }}
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
               et justo sed
-            </p>
+            </motion.p>
             <br />
             <Link
               href="https://www.google.com"
@@ -71,40 +133,119 @@ export default function Introduction() {
               Get a free viewing of our e-book!
             </Link>
           </div>
-        </div>
+        </motion.div>
         <div className="flex w-1/3 items-center justify-center">
-          <img
+          <motion.img
             src="https://freesvg.org/img/1488738791.png"
             alt="E-Book"
             className="h-80"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.8 }}
           />
         </div>
       </div>
       {/* Additional Boxes */}
       <div className="flex items-center justify-center">
-        <div className="relative flex h-96 w-3/5 flex-col items-start justify-center gap-4 bg-yellow-200 p-8">
-          <h1 className="text-4xl font-bold">Pictures</h1>
-          <p className="text-gray-500">Explore our collection of pictures</p>
-          <Link href="/pictures" className="text-blue-500 underline">
+        <motion.div
+          ref={ref2}
+          className="relative flex h-96 w-3/5 flex-col items-start justify-center gap-4 bg-yellow-200 p-8"
+          variants={slideIn}
+          initial="hidden"
+          animate={inView2 ? "visible" : "hidden"}
+        >
+          <motion.h1
+            className="text-4xl font-bold"
+            variants={fadeIn}
+            transition={{ duration: 0.8 }}
+          >
+            Pictures
+          </motion.h1>
+          <motion.p
+            className="text-gray-500"
+            variants={fadeIn}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            Explore our collection of pictures
+          </motion.p>
+          <Link
+            href="/pictures"
+            className="text-blue-500 underline"
+            variants={fadeIn}
+            transition={{ duration: 0.8, delay: 0.4 }}
+          >
             View pictures
           </Link>
-        </div>
+        </motion.div>
       </div>
       <div className="flex items-center justify-center">
-        <div className="relative flex w-full flex-col items-start justify-center gap-4 bg-green-200 p-8">
-          <h1 className="text-4xl font-bold">Box 2</h1>
-          <p className="text-gray-500">Content of Box 2</p>
-        </div>
+        <motion.div
+          ref={ref3}
+          className="relative flex w-full flex-col items-start justify-center gap-4 bg-green-200 p-8"
+          variants={slideIn}
+          initial="hidden"
+          animate={inView3 ? "visible" : "hidden"}
+        >
+          <motion.h1
+            className="text-4xl font-bold"
+            variants={fadeIn}
+            transition={{ duration: 1.5 }}
+          >
+            Box 2
+          </motion.h1>
+          <motion.p
+            className="text-gray-500"
+            variants={fadeIn}
+            transition={{ duration: 2.0, delay: 0.2 }}
+          >
+            Content of Box 2
+          </motion.p>
+        </motion.div>
       </div>
       <div className="flex items-center justify-center">
-        <div className="relative flex h-96 w-96 flex-col items-start justify-center gap-4 bg-blue-200 p-8">
-          <h1 className="text-4xl font-bold">Box 3</h1>
-          <p className="text-gray-500">Content of Box 3</p>
-        </div>
-        <div className="relative flex h-96 w-96 flex-col items-start justify-center gap-4 bg-purple-200 p-8">
-          <h1 className="text-4xl font-bold">Box 4</h1>
-          <p className="text-gray-500">Content of Box 4</p>
-        </div>
+        <motion.div
+          ref={ref4}
+          className="relative flex h-96 w-96 flex-col items-start justify-center gap-4 bg-blue-200 p-8"
+          variants={combination}
+          initial="hidden"
+          animate={inView4 ? "visible" : "hidden"}
+        >
+          <motion.h1
+            className="text-4xl font-bold"
+            variants={fadeIn}
+            transition={{ duration: 1.5 }}
+          >
+            Box 3
+          </motion.h1>
+          <motion.p
+            className="text-gray-500"
+            variants={fadeIn}
+            transition={{ duration: 2.0, delay: 0.2 }}
+          >
+            Content of Box 3
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className="relative flex h-96 w-96 flex-col items-start justify-center gap-4 bg-purple-200 p-8"
+          variants={combination}
+          initial="hidden"
+          animate={inView4 ? "visible" : "hidden"}
+        >
+          <motion.h1
+            className="text-4xl font-bold"
+            variants={fadeIn}
+            transition={{ duration: 1.5 }}
+          >
+            Box 4
+          </motion.h1>
+          <motion.p
+            className="text-gray-500"
+            variants={fadeIn}
+            transition={{ duration: 2.0, delay: 0.2 }}
+          >
+            Content of Box 4
+          </motion.p>
+        </motion.div>
       </div>
     </div>
   );
