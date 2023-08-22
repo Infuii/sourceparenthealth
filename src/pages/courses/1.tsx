b2211613af2fbc9054e2324ffcc567eadb8977a0
BuildTools <unconfigured@null.spigotmc.org>
2023-08-22 16:40:08 -0400

    Updated individual course pages to match course page

--- src/pages/courses/1.tsx
+++ src/pages/courses/1.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { FaCheck, FaTimes, FaHeart, FaBrain, FaFeather } from "react-icons/fa";
+import { FaCheck, FaHeart, FaBrain, FaFeather } from "react-icons/fa";
 import { useInView } from "react-intersection-observer";
 import { useAnimation, motion } from "framer-motion";
 import Link from "next/link";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePublishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
+if (!stripePublishableKey) {
+  throw new Error("Stripe Publishable Key is not set");
+}
 export default function One() {
   const { ref, inView } = useInView({ threshold: 0.1 });
   const animation = useAnimation();
   const [pricingModel, setPricingModel] = useState("monthly");
+  const stripePromise = loadStripe(stripePublishableKey as never);
 
   useEffect(() => {
     if (inView) {
@@ -18,8 +24,32 @@ export default function One() {
     }
   }, [animation, inView]);
 
-  const handlePurchase = () => {
-    // TODO: Add Stripe integration
+  const handlePurchase = async (selectedPriceId: string) => {
+    try {
+      // Call your backend to create the Checkout Session
+      const response = await fetch("../api/create-checkout-session", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ selectedPriceId }),
+      });
+
+      const data: { sessionId: string } = (await response.json()) as never;
+      console.log("Received sessionId from backend:", data.sessionId);
+      const { sessionId } = data;
+
+      // When the customer clicks on the button, redirect them to Checkout.
+      const stripe = await stripePromise;
+      if (stripe !== null) {
+        const { error } = await stripe.redirectToCheckout({ sessionId });
+        if (error) {
+          console.log(error);
+        }
+      }
+    } catch (err) {
+      console.error("There was an error with the purchase process:", err);
+    }
   };
 
   const courses = [
@@ -71,61 +101,90 @@ export default function One() {
   ];
 
   return (
-    <motion.div
-      ref={ref}
-      className="mt-8 flex justify-center gap-4"
-      animate={animation}
-    >
-      {/* Just take the first course and render its box */}
-      {(() => {
-        const course = courses[0];
-        return (
-          <motion.div
-            key={course?.title}
-            className={`relative flex h-full w-full flex-col justify-between rounded-lg border border-t-4 border-black border-green-400 bg-gray-100 p-5 shadow shadow-xl transition-all duration-500 ease-in-out hover:border-8 md:w-1/2`}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.5 }}
-          >
-            <Link href={`/courses/1`}>
-              <div className="flex flex-col items-center justify-start space-y-4">
-                {course?.icon}
-                <h1 className="text-xl font-semibold">{course?.title}</h1>
-                <p className="text-sm">{course?.description}</p>
-                <p className="font-serif text-lg font-bold text-gray-500">
-                  Price:{" "}
-                  {pricingModel === "monthly"
-                    ? course?.monthlyPrice
-                    : course?.yearlyPrice}
-                  <span className="text-sm font-normal">/{pricingModel}</span>
-                </p>
+    <div className="items-center justify-center text-center">
+      <br />
+      <button
+        className={`px-4 py-2 ${
+          pricingModel === "monthly"
+            ? "bg-blue-500 text-white"
+            : "bg-gray-200 text-gray-800"
+        }`}
+        onClick={() => setPricingModel("monthly")}
+      >
+        Monthly
+      </button>
+      <button
+        className={`px-4 py-2 ${
+          pricingModel === "yearly"
+            ? "bg-blue-500 text-white"
+            : "bg-gray-200 text-gray-800"
+        }`}
+        onClick={() => setPricingModel("yearly")}
+      >
+        Yearly
+      </button>
+      <motion.div
+        ref={ref}
+        className="mt-8 flex justify-center gap-4"
+        animate={animation}
+      >
+        {/* Just take the first course and render its box */}
+        {(() => {
+          const course = courses[0];
+          return (
+            <motion.div
+              key={course?.title}
+              className={`relative flex h-full w-full flex-col justify-between rounded-lg border border-t-4 border-black border-green-400 bg-gray-100 p-5 shadow shadow-xl transition-all duration-500 ease-in-out hover:border-8 md:w-1/2`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.5 }}
+            >
+              <Link href={`/courses/1`}>
+                <div className="flex flex-col items-center justify-start space-y-4">
+                  {course?.icon}
+                  <h1 className="text-xl font-semibold">{course?.title}</h1>
+                  <p className="text-sm">{course?.description}</p>
+                  <p className="font-serif text-lg font-bold text-gray-500">
+                    Price:{" "}
+                    {pricingModel === "monthly"
+                      ? course?.monthlyPrice
+                      : course?.yearlyPrice}
+                    <span className="text-sm font-normal">/{pricingModel}</span>
+                  </p>
+                </div>
+              </Link>
+              <div className="py-2">
+                {course?.advantages.map((advantage, index) => (
+                  <motion.div
+                    key={index}
+                    className="flex flex-row items-center space-x-2 py-1"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0.5 }}
+                  >
+                    <FaCheck className="text-sm text-green-500" />
+                    <span className="text-sm text-gray-600">{advantage}</span>
+                  </motion.div>
+                ))}
               </div>
-            </Link>
-            <div className="py-2">
-              {course?.advantages.map((advantage, index) => (
-                <motion.div
-                  key={index}
-                  className="flex flex-row items-center space-x-2 py-1"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ delay: 0.5 }}
+              <div>
+                <button
+                  className="w-full rounded bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition-all duration-500 ease-in-out hover:border-4 hover:border-transparent hover:bg-blue-700 hover:shadow-lg"
+                  onClick={() =>
+                    void handlePurchase(
+                      pricingModel === "monthly"
+                        ? (course?.monthlyPriceId as never)
+                        : (course?.yearlyPriceId as never)
+                    )
+                  }
                 >
-                  <FaCheck className="text-sm text-green-500" />
-                  <span className="text-sm text-gray-600">{advantage}</span>
-                </motion.div>
-              ))}
-            </div>
-            <div>
-              <button
-                className="w-full rounded bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition-all duration-500 ease-in-out hover:border-4 hover:border-transparent hover:bg-blue-700 hover:shadow-lg"
-                onClick={() => handlePurchase()}
-              >
-                Opt in
-              </button>
-            </div>
-          </motion.div>
-        );
-      })()}
-    </motion.div>
+                  Opt in
+                </button>
+              </div>
+            </motion.div>
+          );
+        })()}
+      </motion.div>
+    </div>
   );
 }
--- src/pages/courses/3.tsx
+++ src/pages/courses/3.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { FaCheck, FaTimes, FaHeart, FaBrain, FaFeather } from "react-icons/fa";
+import { FaCheck, FaHeart, FaBrain, FaFeather } from "react-icons/fa";
 import { useInView } from "react-intersection-observer";
 import { useAnimation, motion } from "framer-motion";
 import Link from "next/link";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePublishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
-export default function Three() {
+if (!stripePublishableKey) {
+  throw new Error("Stripe Publishable Key is not set");
+}
+export default function One() {
   const { ref, inView } = useInView({ threshold: 0.1 });
   const animation = useAnimation();
   const [pricingModel, setPricingModel] = useState("monthly");
+  const stripePromise = loadStripe(stripePublishableKey as never);
 
   useEffect(() => {
     if (inView) {
@@ -18,33 +24,79 @@ export default function Three() {
     }
   }, [animation, inView]);
 
-  const handlePurchase = () => {
-    // TODO: Add Stripe integration
+  const handlePurchase = async (selectedPriceId: string) => {
+    try {
+      // Call your backend to create the Checkout Session
+      const response = await fetch("../api/create-checkout-session", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ selectedPriceId }),
+      });
+
+      const data: { sessionId: string } = (await response.json()) as never;
+      console.log("Received sessionId from backend:", data.sessionId);
+      const { sessionId } = data;
+
+      // When the customer clicks on the button, redirect them to Checkout.
+      const stripe = await stripePromise;
+      if (stripe !== null) {
+        const { error } = await stripe.redirectToCheckout({ sessionId });
+        if (error) {
+          console.log(error);
+        }
+      }
+    } catch (err) {
+      console.error("There was an error with the purchase process:", err);
+    }
   };
 
   const courses = [
     {
-      title: "Free Discovery Call",
+      title: "Level 1: Build Your STEM",
       description:
-        "Get a 1-hour discovery call for free, learn more about yourself and your lifestyle.",
-      monthlyPrice: "Free",
-      yearlyPrice: "Free",
+        "Reduce weight and prevent diabetes without medication with one-on-one customized sessions, four one-on-one coaching calls per month.",
+      monthlyPrice: "$400",
+      monthlyFrequency: "month (4 sessions)",
+      yearlyPrice: "$4400",
+      yearlyFrequency: "year ($400 off)",
       advantages: [
-        "Advantage 1",
-        "Advantage 2",
-        "Advantage 3",
-        "Advantage 4",
-        "Advantage 5",
+        "S = Sleep Restore",
+        "T = Toxin Release",
+        "E = Eat to Heal",
+        "M = Move Joyfully",
+        "Reduce Weight",
+        "Prevent Diabetes without Medications",
+        "Quick Support for Acute Issues",
+        "Weekly Group Calls focusing on weight, diabetes",
+        "Weekly Group Calls focusing on fatigue, stress",
       ],
+      monthlyPriceId: "price_1NhIJbLg8nvmDPHiOQigBy1k",
+      yearlyPriceId: "price_1NhIJbLg8nvmDPHibiS7ZTui",
       icon: <FaBrain className="text-4xl text-green-500" />,
     },
     {
-      title: "3-Month Weight Reduction and Diabetes Prevention Program",
-      description: "Reduce weight and prevent diabetes without medication.",
-      monthlyPrice: "$99",
-      yearlyPrice: "$999",
-      advantages: ["Advantage 1", "Advantage 2", "Advantage 3"],
-      disadvantages: ["Disadvantage 1", "Disadvantage 2"],
+      title: "Level 2: GERM Health",
+      description:
+        "Building on STEM concepts with a focus on digestive health and one-on-one customized sessions.",
+      monthlyPrice: "$350",
+      monthlyFrequency: "month for 3 months",
+      yearlyPrice: "$960",
+      yearlyFrequency: "3 months (4 sessions)",
+      advantages: [
+        "G = Gut Health",
+        "E = Emotional Freedom",
+        "R = Relationship Rich",
+        "M = Money Confidence",
+        "Build on STEM Concepts",
+        "Focus on Digestive Health",
+        "Build Habits and Consistency",
+        "Clear Understanding on Diabetes, Digestive Disorders, Unwanted Weight, and Fatty Liver",
+        "Create Own Meal Plans without Compromising on Taste and Family Gatherings",
+      ],
+      monthlyPriceId: "price_1NhIUiLg8nvmDPHijSGeb3Wy",
+      yearlyPriceId: "price_1NhIUiLg8nvmDPHiFSIvpnUt",
       icon: <FaHeart className="text-4xl text-red-500" />,
     },
     {
@@ -52,7 +104,9 @@ export default function Three() {
       description:
         "Go beyond basic habits with one-on-one customized sessions. Focus on thought nourishment, influence at home and work, a healthier money mindset, and abundant energy.",
       monthlyPrice: "$225",
+      monthlyFrequency: "6 months",
       yearlyPrice: "$1260",
+      yearlyFrequency: "per year",
       advantages: [
         "T = Thoughts that Nourish",
         "I = Influence at home and work",
@@ -71,61 +125,90 @@ export default function Three() {
   ];
 
   return (
-    <motion.div
-      ref={ref}
-      className="mt-8 flex justify-center gap-4"
-      animate={animation}
-    >
-      {/* Just take the first course and render its box */}
-      {(() => {
-        const course = courses[2];
-        return (
-          <motion.div
-            key={course?.title}
-            className={`relative flex h-full w-full flex-col justify-between rounded-lg border border-t-4 border-black border-green-400 bg-gray-100 p-5 shadow shadow-xl transition-all duration-500 ease-in-out hover:border-8 md:w-1/2`}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.5 }}
-          >
-            <Link href={`/courses/1`}>
-              <div className="flex flex-col items-center justify-start space-y-4">
-                {course?.icon}
-                <h1 className="text-xl font-semibold">{course?.title}</h1>
-                <p className="text-sm">{course?.description}</p>
-                <p className="font-serif text-lg font-bold text-gray-500">
-                  Price:{" "}
-                  {pricingModel === "monthly"
-                    ? course?.monthlyPrice
-                    : course?.yearlyPrice}
-                  <span className="text-sm font-normal">/{pricingModel}</span>
-                </p>
+    <div className="items-center justify-center text-center">
+      <br />
+      <button
+        className={`px-4 py-2 ${
+          pricingModel === "monthly"
+            ? "bg-blue-500 text-white"
+            : "bg-gray-200 text-gray-800"
+        }`}
+        onClick={() => setPricingModel("monthly")}
+      >
+        Monthly
+      </button>
+      <button
+        className={`px-4 py-2 ${
+          pricingModel === "yearly"
+            ? "bg-blue-500 text-white"
+            : "bg-gray-200 text-gray-800"
+        }`}
+        onClick={() => setPricingModel("yearly")}
+      >
+        Yearly
+      </button>
+      <motion.div
+        ref={ref}
+        className="mt-8 flex justify-center gap-4"
+        animate={animation}
+      >
+        {/* Just take the first course and render its box */}
+        {(() => {
+          const course = courses[2];
+          return (
+            <motion.div
+              key={course?.title}
+              className={`relative flex h-full w-full flex-col justify-between rounded-lg border border-t-4 border-black border-green-400 bg-gray-100 p-5 shadow shadow-xl transition-all duration-500 ease-in-out hover:border-8 md:w-1/2`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.5 }}
+            >
+              <Link href={`/courses/1`}>
+                <div className="flex flex-col items-center justify-start space-y-4">
+                  {course?.icon}
+                  <h1 className="text-xl font-semibold">{course?.title}</h1>
+                  <p className="text-sm">{course?.description}</p>
+                  <p className="font-serif text-lg font-bold text-gray-500">
+                    Price:{" "}
+                    {pricingModel === "monthly"
+                      ? course?.monthlyPrice
+                      : course?.yearlyPrice}
+                    <span className="text-sm font-normal">/{pricingModel}</span>
+                  </p>
+                </div>
+              </Link>
+              <div className="py-2">
+                {course?.advantages.map((advantage, index) => (
+                  <motion.div
+                    key={index}
+                    className="flex flex-row items-center space-x-2 py-1"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0.5 }}
+                  >
+                    <FaCheck className="text-sm text-green-500" />
+                    <span className="text-sm text-gray-600">{advantage}</span>
+                  </motion.div>
+                ))}
               </div>
-            </Link>
-            <div className="py-2">
-              {course?.advantages.map((advantage, index) => (
-                <motion.div
-                  key={index}
-                  className="flex flex-row items-center space-x-2 py-1"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ delay: 0.5 }}
+              <div>
+                <button
+                  className="w-full rounded bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition-all duration-500 ease-in-out hover:border-4 hover:border-transparent hover:bg-blue-700 hover:shadow-lg"
+                  onClick={() =>
+                    void handlePurchase(
+                      pricingModel === "monthly"
+                        ? (course?.monthlyPriceId as never)
+                        : (course?.yearlyPriceId as never)
+                    )
+                  }
                 >
-                  <FaCheck className="text-sm text-green-500" />
-                  <span className="text-sm text-gray-600">{advantage}</span>
-                </motion.div>
-              ))}
-            </div>
-            <div>
-              <button
-                className="w-full rounded bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition-all duration-500 ease-in-out hover:border-4 hover:border-transparent hover:bg-blue-700 hover:shadow-lg"
-                onClick={() => handlePurchase()}
-              >
-                Opt in
-              </button>
-            </div>
-          </motion.div>
-        );
-      })()}
-    </motion.div>
+                  Opt in
+                </button>
+              </div>
+            </motion.div>
+          );
+        })()}
+      </motion.div>
+    </div>
   );
 }
