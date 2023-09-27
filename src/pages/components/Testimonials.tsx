3e48266d938ea8440b1882ae276682dd77e9a897
BuildTools <unconfigured@null.spigotmc.org>
2023-09-27 17:19:01 -0400

    fo rover => for over typing mistake

--- src/pages/components/Testimonials.tsx
+++ src/pages/components/Testimonials.tsx
@@ -247,7 +247,7 @@ const Testimonials: React.FC<TestimonialsProps> = ({ testimonials = [] }) => {
                   >
                     <path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
                   </svg>
-                  I have known Sarah as an astute professional fo rover a decade
+                  I have known Sarah as an astute professional for over a decade
                   now, and a benevolent fellow human being. Committed, sharp,
                   and extremely thorough with her work coupled with risk-taking
                   ability which has alway smade her stand out from her other
